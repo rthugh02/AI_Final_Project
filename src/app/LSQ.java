8a2aec1ab1dcff6658132d5dd84f58420773981f
Read Hughes <hughes.read@gmail.com>
2019-11-06 11:16:36 -0500

    initial commit for LSQ calcFitness function

--- src/app/LSQ.java
+++ src/app/LSQ.java
@@ -10,6 +10,12 @@ public class LSQ
     private Symbol[][] lsqTable;
     private final char startChar;
 
+    //-1 indicates lowest row has not been determined, other values indicate index
+    private int lowestRow = -1;
+    private int lowestColumn = -1;
+
+    private double fitness = -1;
+
     public LSQ(int dimension, Symbol[][] lsqTable, char startChar)
     {
         this.dimension = dimension;
@@ -48,6 +54,21 @@ public class LSQ
     public Symbol getSymbol(int column, int row)
     {
         return lsqTable[column][row];
+    }
+
+    public int getLowestRow() 
+    {
+        return lowestRow == -1 ? null : lowestRow;
+    }
+
+    public int getLowestColumn()
+    {
+        return lowestColumn == -1 ? null : lowestColumn;
+    }
+
+    public double getFitness() 
+    {
+        return fitness;
     }
 
     //initialize the empty cells of a table with random characters
@@ -119,5 +140,82 @@ public class LSQ
 
         //call init again to get a randomized table
         init();
+    }
+    public void calcFitness() 
+    {
+        int repititions = 0;
+        int lowestRowCount = dimension + 1;
+        int rowCountStatics = dimension + 1;
+        int lowestColumnCount = dimension + 1;
+        int columnCountStatics = dimension + 1;
+
+        //counting repitions in each column, and determining column with lowest repitions
+        for(int i = 0; i < dimension; i++) 
+        {
+            HashMap<Symbol, Integer> repititionCounters = new HashMap<Symbol, Integer>(dimension);
+            for(int j = 0; j < dimension; j++)
+                repititionCounters.put(lsqTable[i][j], 1);
+            int localColumnRepitions = 0;
+            int localColumnStatics = 0;
+            for(int j = 0; j < dimension; j++)
+            {
+                if(repititionCounters.get(lsqTable[i][j]) < 1)
+                {
+                    repititions++;
+                    localColumnRepitions++;
+                }
+                else
+                   repititionCounters.put(lsqTable[i][j], 0);
+                if(lsqTable[i][j].isLocked())
+                    localColumnStatics++; 
+            }
+            if(localColumnRepitions < lowestColumnCount)
+            {
+                lowestColumnCount = localColumnRepitions;
+                columnCountStatics = localColumnStatics;
+                this.lowestColumn = i;
+            }
+            else if(localColumnRepitions == lowestColumnCount && localColumnStatics < columnCountStatics)
+            {
+                columnCountStatics = localColumnStatics;
+                this.lowestColumn = i;
+            }
+        }
+
+        //doing the same, but now for the rows
+        for(int i = 0; i < dimension; i++) 
+        {
+            HashMap<Symbol, Integer> repititionCounters = new HashMap<Symbol, Integer>(dimension);
+            for(int j = 0; j < dimension; j++)
+                repititionCounters.put(lsqTable[j][i], 1);
+            int localRowRepitions = 0;
+            int localRowStatics = 0;
+            for(int j = 0; j < dimension; j++)
+            {
+                if(repititionCounters.get(lsqTable[j][i]) < 1)
+                {
+                    repititions++;
+                    localRowRepitions++;
+                }
+                else
+                   repititionCounters.put(lsqTable[j][i], 0);
+                if(lsqTable[j][i].isLocked())
+                    localRowStatics++; 
+            }
+            if(localRowRepitions < lowestRowCount)
+            {
+                lowestRowCount = localRowRepitions;
+                rowCountStatics = localRowStatics;
+                this.lowestRow = i;
+            }
+            else if(localRowRepitions == lowestRowCount && localRowStatics < rowCountStatics)
+            {
+                rowCountStatics = localRowStatics;
+                this.lowestRow = i;
+            }
+        }
+
+        if(repititions != 0)
+            this.fitness = 1/repititions;
     }
 }
